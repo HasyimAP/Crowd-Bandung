b11319785cee8183441d9bab4fdadd99fae1661e
Hasyim <abdillahpronosumarto@gmail.com>
2022-11-24 00:16:47 +0700

    apps update

--- CrowdBandung/app/src/main/java/com/example/crowdbandung/CCTVPage.kt
+++ CrowdBandung/app/src/main/java/com/example/crowdbandung/CCTVPage.kt
@@ -3,10 +3,10 @@ package com.example.crowdbandung
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class CCTVPage : AppCompatActivity() {
+class CctvPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cctvpage)
+        setContentView(R.layout.activity_cctv_page)
 
         supportActionBar?.hide()
     }
