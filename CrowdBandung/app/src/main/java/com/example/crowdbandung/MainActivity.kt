c5aa5dd578db28146169c0d02d1fa1c38ebc42dd
Hasyim <abdillahpronosumarto@gmail.com>
2022-11-25 20:26:03 +0700

    app update

--- CrowdBandung/app/src/main/java/com/example/crowdbandung/MainActivity.kt
+++ CrowdBandung/app/src/main/java/com/example/crowdbandung/MainActivity.kt
@@ -2,12 +2,33 @@ package com.example.crowdbandung
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.crowdbandung.databinding.ActivityMainBinding
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar?.hide()
+    }
+
+    private fun fetchVideoData(): Thread {
+        return Thread {
+            val url = URL("http://cariloka.com/atcscctvindon/cctvatcsindonlengkap/jabar.json")
+            val connection = url.openConnection() as HttpsURLConnection
+
+            if(connection.responseCode == 200) {
+                val inputSystem = connection.inputStream
+                println(inputSystem.toString())
+            }
+            else {
+                binding.textView.text = "Connection failed"
+            }
+        }
     }
 }
